f8ce9045f1df90fd322d01f9e886b77dabc5f5ad
Riajul Islam <riajulpislam@gmail.com>
2023-04-03 15:47:00 +0600

    variable

---
+++ variable/ver.js
@@ -0,0 +1,16 @@
+//"use strict";
+//intro to variable
+
+//variables can store some information
+//we can use that information latter
+//we can change that information latter
+
+//declare a variable
+  var firstNmae = 'pavel';
+
+//use a variable
+console.log(firstNmae);
+
+//change value
+  firstNmae = 'riajul islam';
+console.log(firstNmae)
